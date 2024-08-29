c90871b2d1fe52f0f35d7f6b37b768fa714342a8
Garam Kim <jaju1995@naver.com>
2024-08-30 06:08:45 +0900

    Add #16 - Allow give auth to all access in security config
    인증 기능 구현 전이므로
    모든 인증 설정을 오픈 한다.
    그리고 폼 로그인을 활성화 해서 간단한 로그인 뷰 작성

---
+++ src/main/java/com/fastcampus/board_project/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.fastcampus.board_project.config;
+
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+//      return http
+//              .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // 권한을 받아서 어떤 request든 다 열어줌
+//              .formLogin((formLogin) -> formLogin
+//                  .loginPage("/user/login"))
+//                   .build();
+
+      return http
+          .authorizeHttpRequests(auth -> auth
+              .requestMatchers(
+                  PathRequest.toStaticResources().atCommonLocations()
+              ).permitAll()
+              .requestMatchers(HttpMethod.GET,
+                  "/",
+                  "/articles",
+                  "/articles/search-hashtag").permitAll()
+              .anyRequest().authenticated())
+          .formLogin(Customizer.withDefaults())
+          .logout(logout -> logout
+              .logoutSuccessUrl("/").permitAll()).build();
+  }
+}
