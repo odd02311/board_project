b5ca986cd779df6789554f90628a926b1bd37e1d
Garam Kim <jaju1995@naver.com>
2024-08-29 23:57:44 +0900

    delete #12 - h2-console 옵션 삭제
    h2는 테스트 전용 db,
    서비스 단계에서 따로 사용 X
    그러므로 yaml에서 삭제
    *추가: data.sql 에 mock data 삽입 이후 test 누락 코드 수정

--- src/main/java/com/fastcampus/board_project/BoardProjectApplication.java
+++ src/main/java/com/fastcampus/board_project/BoardProjectApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardProjectApplication {
 
   public static void main(String[] args) {
+
     SpringApplication.run(BoardProjectApplication.class, args);
+
   }
 
 }
