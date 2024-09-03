0113be10824b6c7680d18ae3326f60608bcf8edc
Delavuida <jaju1995@naver.com>
2024-09-03 11:42:00 +0900

    Add & Updte #40 Comment
    댓글 삭제 추가 기능 및 테스트 구현

--- src/main/java/com/fastcampus/board_project/service/ArticleCommentService.java
+++ src/main/java/com/fastcampus/board_project/service/ArticleCommentService.java
@@ -1,25 +1,45 @@
 package com.fastcampus.board_project.service;
 
+import com.fastcampus.board_project.domain.Article;
+import com.fastcampus.board_project.domain.UserAccount;
 import com.fastcampus.board_project.dto.ArticleCommentDto;
 import com.fastcampus.board_project.repository.ArticleCommentRepository;
+import com.fastcampus.board_project.repository.ArticleRepository;
+import com.fastcampus.board_project.repository.UserAccountRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class ArticleCommentService {
 
+    private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
+    private final UserAccountRepository userAccountRepository;
 
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComments(Long articleId) {
-        return List.of();
+        return articleCommentRepository.findByArticle_Id(articleId)
+                .stream()
+                .map(ArticleCommentDto::from)
+                .toList();
     }
 
     public void saveArticleComment(ArticleCommentDto dto) {
+        try{
+            Article article = articleRepository.getReferenceById(dto.articleId());
+            UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
+            articleCommentRepository.save(dto.toEntity(article, userAccount));
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글 작성에 필요한 정보를 찾을 수 없습니다 - {}", e.getLocalizedMessage());
+        }
     }
 
     public void updateArticleComment(ArticleCommentDto dto) {
